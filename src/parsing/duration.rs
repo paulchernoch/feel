89b1fbeed56e108840b45130bc9db4f44644a001
paulchernoch <paul.chernoch@yahoo.com>
2021-02-15 13:28:14 -0500

    Flesh out QName, Duration

--- src/parsing/duration.rs
+++ src/parsing/duration.rs
@@ -2,6 +2,7 @@ use std::fmt;
 use std::cmp;
 use std::ops;
 use std::str::FromStr;
+use chrono::Duration as ChronoDuration;
 use super::duration_parser::parse_duration;
 
 #[derive(Debug, Copy, Clone, PartialEq)]
@@ -14,10 +15,19 @@ pub enum DurationVariety {
   Full
 }
 
+/// Measures a duration of time in years, months, days, hours, minutes and seconds.
+/// Supports arithmetic like adding, subtracting and dividing pairs of Durations,
+/// or multiplying and dividing by scalars.
+/// Durations may be parsed from strings conforming to the XPath syntax.
+/// 
+/// NOTE: The Duration class form Chrono would be adequate if not for the fact that it
+/// does not support durations specified as months, which us understandable, 
+/// as months vary in length and this gets messy.
 #[derive(Debug, Copy, Clone)]
 pub struct Duration {
   /// Restricts which properties may be set to a nonzero value and affects the string formatting.
   variety: DurationVariety,
+  /// If true, the Duration is a positive value, otherwise a negative value.
   positive: bool,
   years: u32,
   months: u32,
@@ -93,7 +103,7 @@ impl Duration {
   /// The resulting Duration will have a single sign, possibly reformulating the values in canonical form.
   /// In canonical form, there are never more than 60 seconds, or more than 60 minutes, or more than 24 hours, etc.
   /// If the canonical transform is required, months will equal 1/12th of a 365 day year in seconds. 
-  pub fn new_from_signed(variety: DurationVariety, years: i32, months: i32, days: i32, hours: i32, minutes: i32, seconds: f32) -> Duration {
+  pub fn new_from_signed(variety: DurationVariety, years: i32, months: i32, days: i32, hours: i32, minutes: i32, seconds: f32) -> Self {
     let mut nonnegative_count = 0_u32;
     let mut nonpositive_count = 0_u32;
     if years >= 0 { nonnegative_count += 1; }
@@ -125,13 +135,21 @@ impl Duration {
       };
     };
     // Some parts are positive and some negative, so we need to create a canonical Duration.
-    let mut as_seconds = ((
+    let as_seconds: f64 = ((
       ((years as f64 + months as f64 / 12.0) * 365.0 + days as f64) * 24.0 
       + hours as f64) * 60.0
       + minutes as f64) * 60.0 
       + seconds as f64;
-    let positive = as_seconds >= 0.0;
-    as_seconds = as_seconds.abs();
+      Duration::new_from_seconds(as_seconds)
+  }
+
+  /// Construct a canonical Duration given a number of seconds,
+  /// such that if the number overflows 60 the extra is converted to minutes.
+  /// If minutes overflows 60, the extra is converted to hours.
+  /// etc. Months are taken as 365/12 days.
+  pub fn new_from_seconds(seconds: f64) -> Self {
+    let positive = seconds >= 0.0;
+    let mut as_seconds = seconds.abs();
     const YEAR_SECONDS: f64 = 60.0 * 60.0 * 24.0 * 365.0;
     const MONTH_SECONDS: f64 = YEAR_SECONDS / 12.0;
     const DAY_SECONDS: f64 = 60.0 * 60.0 * 24.0;
@@ -150,6 +168,11 @@ impl Duration {
     Duration::new(positive, years_can as u32, months_can as u32, days_can as u32, hours_can as u32, minutes_can as u32, seconds_can as f32)
   }
 
+  pub fn new_from_nanoseconds(nano_seconds: i64) -> Self {
+    let seconds = nano_seconds as f64 / 1.0e9;
+    Self::new_from_seconds(seconds)
+  }
+
   /// Are all components of the duration zero or not?
   pub fn is_zero(&self) -> bool {
     self.years == 0 && self.months == 0 && self.days == 0 && self.hours == 0 && self.minutes == 0 && self.seconds == 0.0
@@ -159,7 +182,11 @@ impl Duration {
     self.hours == 0 && self.minutes == 0 && self.seconds == 0.0
   }
 
-  /// Convert the duration into an approximate number of seconds, 
+  pub fn are_year_month_zero(&self) -> bool {
+    self.years == 0 && self.months == 0
+  }
+
+  /// Convert the duration into an approximate and signed number of seconds, 
   /// treating a year as 365 days and a month as 1/12th of a year.
   pub fn total_seconds(&self) -> f32 {
     let sign:f32 = if self.positive { 1.0 } else { -1.0 };
@@ -178,6 +205,8 @@ impl Duration {
     Duration::new_year_month(self.positive, self.years, months)
   }
 
+  /// Convert to a DayTime variety Duration by piling all the years, and months into the days field.
+  /// For example, if the Duration was for one year, it would become a 365 day Duration.
   pub fn as_day_time(&self, days_in_month: f32) -> Duration {
     let days_float: f32 = ((self.years as f32) * 12.0 + (self.months as f32)) * days_in_month + self.days as f32;
     let days = days_float as u32;
@@ -284,11 +313,102 @@ impl ops::Sub<Duration> for Duration {
   }
 }
 
+/// Divide two Durations to get a scalar.
+impl ops::Div<Duration> for Duration {
+  type Output = f32;
+
+  fn div(self, rhs: Self) -> Self::Output {
+      if rhs.is_zero() {
+          panic!("Cannot divide by zero-valued `Duration`!");
+      };
+      self.total_seconds()  / rhs.total_seconds()
+  }
+}
+
+impl ops::Div<f32> for Duration {
+  type Output = Duration;
+
+  /// Divide a Duration by a scalar and derive a new Duration.
+  /// A YearMonth type Duration will remain so, and round down to a whole month. 
+  /// Months will be treated as one twelfth of a year.
+  fn div(self, rhs: f32) -> Self::Output {
+      if rhs == 0.0 {
+          panic!("Cannot divide `Duration` by zero!");
+      };
+      let positive = self.positive == (rhs >= 0.0);
+      let seconds: f64 = self.total_seconds() as f64 / rhs as f64;
+      
+      match self.variety {
+        DurationVariety::YearMonth => {
+          let total_months: u32 = ((self.years as f32 * 12.0 + self.months as f32 + self.days as f32 * 12.0 / 365.0) / rhs.abs()).floor() as u32;
+          let years = total_months / 12;
+          let months = total_months % 12;
+          Duration::new_year_month(positive, years, months)
+        },
+        DurationVariety::DayTime => Duration::new_from_seconds(seconds).as_day_time(365.0/12.0),
+        DurationVariety::Full => Duration::new_from_seconds(seconds)
+      }
+  }
+}
+
+impl ops::Mul<f32> for Duration {
+  type Output = Duration;
+
+  /// Multiply a Duration by a scalar and derive a new Duration.
+  /// A YearMonth type Duration will remain so, and round down to a whole month. 
+  /// Months will be treated as one twelfth of a year.
+  fn mul(self, rhs: f32) -> Self::Output {
+      if rhs == 0.0 {
+        return Duration::new_zero(self.variety);
+      };
+      if rhs == 1.0 {
+        return self;
+      }
+      let positive = self.positive == (rhs >= 0.0);
+      let seconds: f64 = self.total_seconds() as f64 * rhs as f64;
+      
+      match self.variety {
+        DurationVariety::YearMonth => {
+          let total_months: u32 = ((self.years as f32 * 12.0 + self.months as f32 + self.days as f32 * 12.0 / 365.0) * rhs.abs()).floor() as u32;
+          let years = total_months / 12;
+          let months = total_months % 12;
+          Duration::new_year_month(positive, years, months)
+        },
+        DurationVariety::DayTime => Duration::new_from_seconds(seconds).as_day_time(365.0/12.0),
+        DurationVariety::Full => Duration::new_from_seconds(seconds)
+      }
+  }
+}
+
+impl ops::Neg for Duration {
+  type Output = Duration;
+
+  fn neg(self) -> Duration {
+      self.negate()
+  }
+}
+
+impl From<ChronoDuration> for Duration {
+  fn from(item: ChronoDuration) -> Self {
+    let nano_seconds = item.num_nanoseconds().unwrap();
+    Duration::new_from_nanoseconds(nano_seconds)
+  }
+}
+
+impl From<Duration> for ChronoDuration {
+  fn from(item: Duration) -> ChronoDuration {
+    let nanoseconds = (item.total_seconds() as f64 * 1.0e9).round() as i64;
+    ChronoDuration::nanoseconds(nanoseconds)
+  }
+}
+
+/////////////// TESTS /////////////////
 
 #[cfg(test)]
 mod tests {
   use super::Duration;
   use std::str::FromStr;
+  use chrono::Duration as ChronoDuration;
 
   #[test]
   fn test_year_month_to_string() {
@@ -351,6 +471,52 @@ mod tests {
   }
 
   #[test]
+  fn test_duration_by_duration_div() {
+    assert_eq!(
+      365.0, 
+      Duration::new_year_month(true, 1_u32, 0_u32) / Duration::new_day_time(true, 1_u32, 0_u32, 0_u32, 0.0), 
+    );
+  }
+
+  #[test]
+  fn test_duration_by_scalar_div() {
+    assert_eq!(
+      Duration::new_year_month(true, 0_u32, 9_u32), 
+      Duration::new_year_month(true, 1_u32, 6_u32) / 2.0, 
+    );
+    // Divide a year down to an hour
+    assert_eq!(
+      Duration::from_str("PT1H").unwrap(), 
+      Duration::from_str("P1Y").unwrap() / (365.0 * 24.0), 
+    );
+
+    // Divide by one twelfth, the same as multiplying by 12.
+    assert_eq!(
+      Duration::from_str("P1Y").unwrap(), 
+      Duration::from_str("P1M").unwrap() / (1.0/12.0), 
+    );
+  }
+
+  #[test]
+  fn test_duration_by_scalar_mul() {
+    assert_eq!(
+      Duration::new_year_month(true, 3_u32, 0_u32), 
+      Duration::new_year_month(true, 1_u32, 6_u32) * 2.0, 
+    );
+    // Reduce a year down to an hour
+    assert_eq!(
+      Duration::from_str("PT1H").unwrap(), 
+      Duration::from_str("P1Y").unwrap() * (1.0 / (365.0 * 24.0)), 
+    );
+
+    // Multiply by 12.
+    assert_eq!(
+      Duration::from_str("P1Y").unwrap(), 
+      Duration::from_str("P1M").unwrap() * 12.0, 
+    );
+  }
+
+  #[test]
   fn test_as_year_month() {
     assert_eq!(
       Duration::new_year_month(true, 1u32, 0u32), 
@@ -371,6 +537,18 @@ mod tests {
     assert_eq!(
       Duration::new_year_month(true, 1u32, 3u32), 
       Duration::from_str("P1Y3M").unwrap(), 
+    );
+  }
+
+
+  #[test]
+  fn test_duration_round_trip() {
+    let ten_seconds_a = Duration::from_str("PT10S").unwrap();
+    let ten_seconds_b: ChronoDuration = ten_seconds_a.into();
+    let ten_seconds_c: Duration = ten_seconds_b.into(); 
+    assert_eq!(
+      ten_seconds_a, 
+      ten_seconds_c, 
     );
   }
 }
--- src/parsing/qname.rs
+++ src/parsing/qname.rs
@@ -1,13 +1,39 @@
 use std::fmt;
+use std::ops;
+use std::str::FromStr;
 
-#[derive(Clone, Debug)]
+/// A qualified name, consisting of a list of strings.
+#[derive(Clone, Debug, Hash)]
 pub struct QName {
   parts: Vec<String>
+}
+
+impl QName {
+  fn new<S>(name: S) -> Self where S: Into<String> {
+    QName { parts: vec![name.into()] }
+  }
 }
 
 impl fmt::Display for QName {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
     write!(f, "{}", self.parts.join(" "))
+  }
+}
+
+#[derive(Debug, Clone)]
+pub struct ParseQNameError;
+
+impl fmt::Display for ParseQNameError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "unable to parse QName from string")
+    }
+}
+
+impl FromStr for QName {
+  type Err = ParseQNameError;
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    Result::Ok(QName { parts : s.split(" ").map(|s| s.to_string()).collect() })
   }
 }
 
@@ -18,14 +44,31 @@ impl PartialEq for QName {
   }
 }
 
-impl Eq for QName {}
+impl Eq for &QName {}
+
+impl<S> ops::Add<S> for &QName where S: Into<String> {
+  type Output = QName;
+  fn add(self, rhs: S) -> QName {
+    let mut new_parts = self.parts.clone();
+    new_parts.push(rhs.into());
+    QName { parts: new_parts }
+  }
+}
 
 #[cfg(test)]
 mod tests {
   use super::QName;
+  use std::collections::hash_map::DefaultHasher;
+  use std::hash::{Hash, Hasher};
 
   macro_rules! vec_of_strings {
     ($($x:expr),*) => (vec![$($x.to_string()),*]);
+  }
+
+  fn calculate_hash<T: Hash>(t: &T) -> u64 {
+    let mut s = DefaultHasher::new();
+    t.hash(&mut s);
+    s.finish()
   }
 
   #[test]
@@ -37,5 +80,24 @@ mod tests {
     assert_eq!(q1, q2);
     assert_ne!(q1, q3);
     assert_ne!(q1, q4);
+  }
+
+  #[test]
+  fn test_qname_hash() {
+    let q1 = &QName::new("Sam") + "Hill";
+    let q2 = &QName::new("Sam") + "Hill";
+    assert_eq!(
+      calculate_hash(&q1),
+      calculate_hash(&q2)
+    )
+  }
+
+  #[test]
+  fn test_qname_fromstr() {
+    let q1: QName = "John Smith".parse().unwrap();
+    assert_eq!(
+      QName { parts: vec_of_strings!["John", "Smith"] },
+      q1,
+    )
   }
 }
